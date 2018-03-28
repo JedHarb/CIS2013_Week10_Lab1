3e8f082faefbe200d128f76ed326dbbe2ba88fe5
JedHarb <jedharb@gmail.com>
2018-03-28 18:45:42 -0500

    in-class work for week 10

---
+++ CIS2013_Week10_Lab1/Source.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstring>
+#include<string> 
+using namespace std;
+
+int main() {
+
+
+
+
+	
+	cout << endl << " End of program ";
+	char keepAlive = 'y';
+	cin >> keepAlive; //keep the program from closing the window until the user inputs something
+	return 0;
+}
